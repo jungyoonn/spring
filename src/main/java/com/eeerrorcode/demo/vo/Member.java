5b34c0e4a759993b58532e3cb28cb6ada85a3f3e
jungyoonn <sophia7625@naver.com>
2024-12-09 11:44:25 +0900

    springboot first commit

---
+++ src/main/java/com/eeerrorcode/demo/vo/Member.java
@@ -0,0 +1,37 @@
+package com.eeerrorcode.demo.vo;
+
+import java.lang.System.Logger.Level;
+import java.util.logging.Logger;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Builder
+@Slf4j
+public class Member {
+    private String id;
+    private String pw;
+    private String name;
+
+    private static final Logger logger = Logger.getLogger("com.eeerrorcode.demo.vo.Member"); // logger는 보통 static final로 만듭니다
+    private static final java.lang.System.Logger logger2 = System.getLogger("com.eeerrorcode.demo.vo.Member");
+
+    public static void main(String[] args) {
+        Member member = Member.builder()
+        .id("abcd")
+        .pw("1234")
+        .name("이름")
+        .build();
+        System.out.println(member); // Member(id=abcd, pw=1234, name=이름
+
+        logger.info(member.toString()); // 12월 09, 2024 11:13:33 오전 com.eeerrorcode.demo.vo.Member main
+                                        // INFO: Member(id=abcd, pw=1234, name=이름)
+        
+        logger2.log(Level.INFO, member); // logger.info()랑 로그 출력 결과가 같다
+        
+        log.info(member.toString()); // Slf4j가 만들어 준 로그 객체 (Slf4j는 lombok이 만들어 줬음!)
+        // 11:22:23.531 [main] INFO com.eeerrorcode.demo.vo.Member -- Member(id=abcd, pw=1234, name=이름)
+    }
+}
