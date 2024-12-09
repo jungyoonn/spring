38c83339712ae9bd53d5222200e99a2f811b7ce3
jungyoonn <sophia7625@naver.com>
2024-12-09 16:49:33 +0900

    어노테이션으로 서비스 만들기

---
+++ src/main/java/com/eeerrorcode/demo/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.eeerrorcode.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eeerrorcode.demo.mapper.MemberMapper;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class MemberService {
+    @Autowired
+    private MemberMapper mapper;
+    
+    public void selectNow() {
+        log.error(mapper.selectNow());
+    }
+}
