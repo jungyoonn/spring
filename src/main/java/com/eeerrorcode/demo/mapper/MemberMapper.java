4f4a4645b5ec6191151fe25609c38ca7ae832558
jungyoonn <sophia7625@naver.com>
2024-12-09 16:49:19 +0900

    config 없이 mapper 만들기

---
+++ src/main/java/com/eeerrorcode/demo/mapper/MemberMapper.java
@@ -0,0 +1,10 @@
+package com.eeerrorcode.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MemberMapper {
+    @Select("select now()")
+    String selectNow();
+}
