702f84ad49b81c515bbd600b4e77531f97f6b05a
jungyoonn <sophia7625@naver.com>
2024-12-09 12:32:48 +0900

    springboot 환경설정

--- src/main/java/com/eeerrorcode/demo/vo/Member.java
+++ src/main/java/com/eeerrorcode/demo/vo/Member.java
@@ -1,6 +1,7 @@
 package com.eeerrorcode.demo.vo;
 
 import java.lang.System.Logger.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 import lombok.Builder;
@@ -17,6 +18,7 @@ public class Member {
 
     private static final Logger logger = Logger.getLogger("com.eeerrorcode.demo.vo.Member"); // logger는 보통 static final로 만듭니다
     private static final java.lang.System.Logger logger2 = System.getLogger("com.eeerrorcode.demo.vo.Member");
+    private static final org.apache.logging.log4j.Logger logger3 = org.apache.logging.log4j.LogManager.getLogger(Member.class);
 
     public static void main(String[] args) {
         Member member = Member.builder()
@@ -33,5 +35,8 @@ public class Member {
         
         log.info(member.toString()); // Slf4j가 만들어 준 로그 객체 (Slf4j는 lombok이 만들어 줬음!)
         // 11:22:23.531 [main] INFO com.eeerrorcode.demo.vo.Member -- Member(id=abcd, pw=1234, name=이름)
+        // @Slf4j 대신 @Log4j2를 추가하고 출력해도 같은 결과가 나온다
+
+        logger3.info(member.toString()); // 얘도 Slf4j랑 같은 결과
     }
 }
